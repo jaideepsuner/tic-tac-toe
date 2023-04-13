9b2ef8797725ccd8a2fce5b2cc6266f5a078d31c
jaideepsuner <end93@proton.me>
2023-04-13 19:20:41 +0530

    checkWin() & gameOver() working

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let currentPlayer = 'X'
 let gameBoard = ['', '', '', '', '', '', '', '', '']
+let isOver = false
 
 function handleBoxClick(event) {
   const clickedBox = event.target
   const clickedBoxIndex = clickedBox.getAttribute('data-index')
 
-  if (gameBoard[clickedBoxIndex] !== '') {
+  if (isOver) {
     return
   }
 
@@ -13,6 +14,56 @@ function handleBoxClick(event) {
   clickedBox.innerHTML = currentPlayer
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
   console.log(gameBoard)
+  checkWin()
+}
+
+const checkWin = () => {
+  if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+    if (gameBoard[0] !== '') {
+      gameOver()
+    }
+  }
+  if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+    if (gameBoard[3] !== '') {
+      gameOver()
+    }
+  }
+  if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8]) {
+    if (gameBoard[6] !== '') {
+      gameOver()
+    }
+  }
+  if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+    if (gameBoard[0] !== '') {
+      gameOver()
+    }
+  }
+  if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+    if (gameBoard[1] !== '') {
+      gameOver()
+    }
+  }
+  if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8]) {
+    if (gameBoard[2] !== '') {
+      gameOver()
+    }
+  }
+  if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+    if (gameBoard[0] !== '') {
+      gameOver()
+    }
+  }
+  if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+    if (gameBoard[2] !== '') {
+      gameOver()
+    }
+  }
+}
+
+const gameOver = () => {
+  isOver = true
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
+  console.log(`player ${currentPlayer} won`)
 }
 
 const boxes = document.getElementsByClassName('box')
